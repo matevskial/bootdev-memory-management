f81cd12c706cf0ee81a1e433c1ed83e537171e8b
Aleksandar Matevski <matevskial@gmail.com>
2024-12-10 22:52:48 +0100

    Add examples of printf for char, int, float, char*

--- main.c
+++ main.c
@@ -2,5 +2,13 @@
 
 int main(void) {
     printf("Hello, World!\n");
+    int max_x = 0;
+    char dir = 'r';
+    float health_damage_percentage = 0.2;
+    char *banner_text = "Banner text";
+    printf("Max x = %d\n", max_x);
+    printf("dir = %c\n", dir);
+    printf("health_damage_percentage = %f\n", health_damage_percentage);
+    printf("banner_text = %s\n", banner_text);
     return 0;
 }
